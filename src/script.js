96c2317fb3c68e85839320b95970bb3afbef9693
kierab <78439567+kierabon@users.noreply.github.com>
2021-03-25 20:28:50 -0500

    java working
    - changed font
    -added weather functions

--- src/script.js
+++ src/script.js
@@ -27,4 +27,51 @@ let month = months[now.getMonth()];
 let time= document.querySelector("#date");
 time.innerHTML =`${day} ${month} ${date}, ${year} ${hours}:${minutes}`;
 }
+
+function showWeather(response) {
+  let temperatureNow = document.querySelector("#temperature");
+  let tempNow = Math.round(response.data.main.temp);
+  let city = response.data.name;
+  let cityNow = document.querySelector("#city");
+  let humid = document.querySelector("#humidity");
+  let humidity = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  let windSpeed = response.data.wind.speed;
+  let description = document.querySelector("#description");
+  let weatherDescription = response.data.weather[0].description;
+
+  description.innerHTML = `${weatherDescription}`;
+  wind.innerHTML = `Wind Speed: ${windSpeed}m/s`;
+  humid.innerHTML = `Humidity: ${humidity}%`;
+  cityNow.innerHTML = `${city}`;
+  temperatureNow.innerHTML = `${tempNow}`;
+}
+
+function weatherLocation(position) {
+  let apiKey = "39a4dba5764c859c9c8cade7545d15da";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showWeather);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(weatherLocation);
+}
+
+function getCitySearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let city = document.querySelector("#city");
+  let citySearch = searchInput.value;
+  let apiKey = "39a4dba5764c859c9c8cade7545d15da";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&units=metric&appid=${apiKey}`;
+  city.innerHTML = `${citySearch}`;
+  axios.get(url).then(showWeather);
+}
 getDate();
+let button = document.querySelector("#location-button");
+let search =document.querySelector("#search-box");
+
+search.addEventListener("submit", getCitySearch);
+button.addEventListener("click",getCurrentPosition);
